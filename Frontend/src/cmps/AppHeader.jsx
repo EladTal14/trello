b67bd878f8184461439ab4c9d8db9aba576006ce
Basya111 <mail4basy@gmail.com>
2021-01-12 19:34:07 +0200

    added routing

--- Frontend/src/cmps/AppHeader.jsx
+++ Frontend/src/cmps/AppHeader.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 export function AppHeader(props) {
   return (
     <header className="app-header">
-      <h1>header</h1>
+      <h1>Tello header</h1>
+      <nav>
+        <ul className="header-list flex justify-center">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/boards">Boards</Link></li>
+          <li><Link to="/board">Board</Link></li>
+          <li><Link to="/board/edit/c104">Edit Todo</Link></li>
+        </ul>
+      </nav>
     </header>
   )
 }
