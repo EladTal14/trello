22c012eed299bcca0617cb485fcd5ab234c5aeab
Elad Tal <eladtal14@gmail.com>
2021-01-13 14:48:24 +0200

    uop

--- Frontend/src/cmps/CardPreview.jsx
+++ Frontend/src/cmps/CardPreview.jsx
@@ -19,14 +19,13 @@ export class CardPreview extends Component {
         const { card, index } = this.props
         return (
             <div className="card-preview">
-
+                {this.state.isDetailsShown &&
+                    <React.Fragment>
+                        <div className="modalcover" onClick={(ev) => this.toggleDetails(ev, true)}> </div>
+                        <CardDetails card={card} />
+                    </React.Fragment>}
 
                 <Draggable key={card.id} draggableId={card.id} index={index}>
-                    {this.state.isDetailsShown &&
-                        <React.Fragment>
-                            <div className="modalcover" onClick={(ev) => this.toggleDetails(ev, true)}> </div>
-                            <CardDetails card={card} />
-                        </React.Fragment>}
                     {(provided, snapshot) => (
                         <div
                             ref={provided.innerRef}
