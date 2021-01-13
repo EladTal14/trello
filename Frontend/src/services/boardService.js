8d4102398d9bbe1f77e749fe79bf496b2654b5a3
Basya111 <mail4basy@gmail.com>
2021-01-13 09:24:01 +0200

    added board service

--- Frontend/src/services/boardService.js
+++ Frontend/src/services/boardService.js
@@ -208,15 +208,40 @@ const gBoard = {
 
 export const boardService = {
     query,
+    remove,
+    save,
+    getBoardById
 
 }
 
 
 const baseUrl = 'http://localhost:3030/api/board';
 
-async function query() {
-    // const res = await axios.get(baseUrl);
+async function query(boardId) {
+    // const res = await axios.get(`${baseUrl}/${boardId}`);
     // return res.data;
     return gBoard.board
 }
+
+function remove(boardId) {
+    return axios.delete(`${baseUrl}/${boardId}`)
+}
+
+
+async function save(board) {
+    if (board._id) {
+        return axios.put(`${baseUrl}/${board._id}`, board)
+    }
+    else {
+        const res = await axios.post(`${baseUrl}`, board);
+        const savedBoard = res.data;
+        return savedBoard;
+    }
+}
+
+async function getBoardById(boardId) {
+    const res = await axios.get(`${baseUrl}/${boardId}`);
+    return res.data;
+}
+
 
--- Frontend/src/store/actions/boardAction.js
+++ Frontend/src/store/actions/boardAction.js
@@ -12,3 +12,28 @@ export  function loadBoard() { // Action Creator
 
     }
 }
+
+export  function saveBoard(board) { 
+    return async (dispatch) => {
+        const saveBoard = await boardService.query(board)
+        const action = {
+            type: (board._id) ? 'SAVE_BOARD' : 'ADD_BOARD',
+            board: saveBoard
+          }
+        dispatch(action)
+
+    }
+}
+
+export  function saveBoard(board) { 
+    return async (dispatch) => {
+        const saveBoard = await boardService.query(board)
+        const action = {
+            type: (board._id) ? 'SAVE_BOARD' : 'ADD_BOARD',
+            board: saveBoard
+          }
+        dispatch(action)
+
+    }
+}
+
