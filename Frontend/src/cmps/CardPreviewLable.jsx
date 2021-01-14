495a8e105c603a2bd482bb3416535d8cec711776
Elad Tal <eladtal14@gmail.com>
2021-01-14 18:48:30 +0200

    sadasd

--- Frontend/src/cmps/CardPreviewLable.jsx
+++ Frontend/src/cmps/CardPreviewLable.jsx
@@ -17,7 +17,7 @@ export class CardPreviewLable extends Component {
         const { isLableTitleShown } = this.state
         return (
             <div>
-                {card.labels && <div className="label-conyainer flex">
+                {card.labels && <div className="label-container flex">
                     {card.labels.map(label => {
                         return <div key={label.id} style={{ backgroundColor: label.color }} className={isLableTitleShown ? 'card-label-open' : 'card-label'} onClick={this.toggleLableTitle}>
                             {isLableTitleShown && label.title}
