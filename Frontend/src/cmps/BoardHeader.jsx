ab7a30cb3f9bdbf39e9f90dc59fac95c1f526788
Basya111 <mail4basy@gmail.com>
2021-01-15 20:07:04 +0200

    added create board

--- Frontend/src/cmps/BoardHeader.jsx
+++ Frontend/src/cmps/BoardHeader.jsx
@@ -1,17 +1,59 @@
 import React, { Component } from 'react'
-import { BoardFilter } from './BoardFilter'
+// import { BoardFilter } from './BoardFilter'
 import { utilService } from '../services/utilService'
+import { saveBoard } from '../store/actions/boardAction'
+import { connect } from 'react-redux'
 
-export class BoardHeader extends Component {
+export class _BoardHeader extends Component {
+
+  state = {
+    board: {
+        title: this.props.board.title,
+
+    }
+}
+
+// componentDidMount() {
+//     const boardId = this.props.board.id
+//     const boardIdx = board.boards.findIndex(board => board.id === boardId)
+//     this.setState({ boardIdx })
+//     // document.addEventListener('click', this.handleClickOutside, true);
+// }
+
+handleInput = ({ target }) => {
+    const { name } = target
+    const value = target.value
+    this.setState(prevState => {
+        return {
+            board: {
+                ...prevState.board,
+                [name]: value
+            }
+        }
+    })
+}
+
+onSaveTitle = async (ev) => {
+  ev.preventDefault()
+  const { board } = this.props
+  const { title } = this.state.board
+  board.title = title
+  await this.props.saveBoard(board)
+}
 
 
   render() {
-    const { title, members } = this.props
+    const { members } = this.props
+    const { board } = this.state
     // console.log('memebers...', members)
     return (
       <header className="board-header flex space-between">
         <div className="header-options flex">
-          <h2>{title}</h2>
+          {/* <h2>{title}</h2> */}
+          <form onSubmit={this.onSaveTitle} onBlur={this.onSaveTitle} className="board-title-form flex space-between">
+                <input type="text" name="title" value={board.title} onChange={this.handleInput}
+                    className="board-title-input" placeholder="Enter board title..." autoComplete="off" />
+            </form>
           <button className="board-header-btn flex space-around">
             <h3>Statistics</h3>
             <img src="https://res.cloudinary.com/basimgs/image/upload/v1610626728/pie-chart_fnvwct.png" alt="" />
@@ -35,5 +77,20 @@ export class BoardHeader extends Component {
       </header>
     )
   }
-
 }
+
+const mapStateToProps = state => {
+  return {
+      board: state.boardModule.currBoard,
+      // filterBy: state.boardModule.filterBy,
+      // loggedInUser: state.userModule.loggedInUser,
+  }
+}
+
+const mapDispatchToProps = {
+  saveBoard,
+}
+
+
+
+export const BoardHeader = connect(mapStateToProps, mapDispatchToProps)(_BoardHeader);
