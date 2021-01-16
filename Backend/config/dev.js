d5f25754f87330c3c378d9fd4b22060ff658944e
Elad Tal <eladtal14@gmail.com>
2021-01-16 11:09:39 +0200

    merge day 5.1

--- Backend/config/dev.js
+++ Backend/config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'dbURL': 'mongodb://localhost:27017',
-}
+  // 'dbURL': 'mongodb://localhost:27017',
+  'dbURL': 'mongodb+srv://Elad:E8XUm7foss9r7rJe@cluster0.vsm2e.mongodb.net/trello_db?retryWrites=true&w=majority'
+}//elad//E8XUm7foss9r7rJe
