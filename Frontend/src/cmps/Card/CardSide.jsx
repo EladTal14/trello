0bd417583e2089a05eba8d34ca2932b190afb83f
Basya111 <mail4basy@gmail.com>
2021-01-17 09:08:28 +0200

    nada

--- Frontend/src/cmps/Card/CardSide.jsx
+++ Frontend/src/cmps/Card/CardSide.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+// import Calendar from 'react-calendar';
+// import 'react-calendar/dist/Calendar.css';
 import { cardService } from '../../services/cardService';
 import { CardLabels } from './CardLabels';
 
