27c96e41d7c478e1a75f4e0d1f9a7a8dc75729d1
Elad Tal <eladtal14@gmail.com>
2021-01-18 10:56:28 +0200

    day 7.3

--- Frontend/src/cmps/BoardPreview.jsx
+++ Frontend/src/cmps/BoardPreview.jsx
@@ -5,8 +5,7 @@ export function BoardPreview({ board }) {
     return (
         <Link to={`/board/${board._id}`}>
             <div style={{
-                backgroundImage: `url(${board.style.backgroundImage})`, backgroundColor: '#6DB3F2',
-                backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'
+                backgroundImage: `url(${board.style.backgroundImage})`,
             }} className="board-preview">
                 <h1 >{board.title}</h1>
             </div>
