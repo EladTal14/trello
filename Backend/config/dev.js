f06892fa83b2c8bed735eb87f098f9e756636100
Basya111 <mail4basy@gmail.com>
2021-01-19 09:07:09 +0200

    l

--- Backend/config/dev.js
+++ Backend/config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   // 'dbURL': 'mongodb://localhost:27017',
-  'dbURL': 'mongodb+srv://Elad:E8XUm7foss9r7rJe@cluster0.vsm2e.mongodb.net/trello_db?retryWrites=true&w=majority'
+  'dbURL': 'mongodb+srv://basya111:eKRzTqKT3WDDrSTX@cluster0.vsm2e.mongodb.net/trello_db?retryWrites=true&w=majority'
 }//Elad//E8XUm7foss9r7rJe
 //eKRzTqKT3WDDrSTX basya
 // g6OSgswcEYsOKzFW Adir
