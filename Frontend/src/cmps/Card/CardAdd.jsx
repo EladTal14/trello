b37676043ace272f252363f8fef935d4ff576d10
Elad Tal <eladtal14@gmail.com>
2021-01-22 14:41:25 +0200

    fixed bugs

--- Frontend/src/cmps/Card/CardAdd.jsx
+++ Frontend/src/cmps/Card/CardAdd.jsx
@@ -1,69 +1,55 @@
 import { Component } from 'react'
 import { utilService } from '../../services/utilService.js'
+import { connect } from 'react-redux'
+import { toggleAddCard } from '../../store/actions/cardAction.js'
+import { eventBusService } from '../../services/eventBusService.js'
 
 export class CardAdd extends Component {
     state = {
         isAddOpen: false,
-        card: {
-            title: '',
-        }
+    }
+    componentDidMount() {
+        this.eventBusTerminate = eventBusService.on('close-card-add', this.closeInput)
     }
 
+    componentWillUnmount() {
+        this.eventBusTerminate()
+    }
     openInput = () => {
         this.setState({ isAddOpen: true }, () => {
             this.props.onScroll(null, 1000)
+            eventBusService.emit('open-card-add', { isAddOpen: true, groupId: this.props.groupId })
         })
     }
 
-    closeInput = () => {
-        this.setState({ isAddOpen: false, card: { title: '' } }, () => setTimeout(() => {
-            this.props.onScroll(null, 1000)
+    closeInput = ({ groupId }) => {
+        this.setState({ isAddOpen: false }, () => setTimeout(() => {
+            if (this.props.groupId === groupId)
+                this.props.onScroll(null, 1000)
         }, 150))
     }
 
-    onSaveCard = (ev) => {
-        ev.preventDefault()
-        if (!this.state.card.title) return
-        const card = { title: this.state.card.title, id: utilService.makeId(), members: [], labels: [] }
-        this.props.onAddCard(card, this.props.groupId)
-        this.setState(
-            {
-                card: { title: '' },
-            }, () => this.closeInput())
-    }
-
-    handleInput = ({ target }) => {
-        const { name } = target
-        const { value } = target
-        this.setState(prevState => {
-            return {
-                card: {
-                    ...prevState.card,
-                    [name]: value
-                }
-            }
-        })
-    }
-
     render() {
-        const { isAddOpen, card } = this.state
-        return <section className="add-card-container" onBlur={this.closeInput} >
-            {isAddOpen &&
-                <form onSubmit={() => { return }} className="new-card-form">
-                    <textarea type="text" name="title" value={card.title} onChange={this.handleInput}
-                        className="my-input" placeholder="Enter a card title..." autoFocus required
-                        cols="35" rows="4"></textarea>
-                    <div className="flex">
-                        <button type="submit" onMouseDown={this.onSaveCard} className="submit-card-btn">Add card</button>
-                        <button onClick={this.closeInput} className="close-input-btn" >
-                            <img src="https://res.cloudinary.com/basimgs/image/upload/v1610646476/close_voj9v3.png" alt="" />
-                        </button>
-                    </div>
-                </form>
-            }
-            {!isAddOpen && <button className="add-card-btn" onClick={this.openInput}>
-                <img src="https://res.cloudinary.com/basimgs/image/upload/v1610625350/plus_ljzrkm.png" alt="" />
-                <span>Add another card</span></button>}
-        </section>
+        const { isAddOpen } = this.state
+        return (
+            <section className="add-card-container" >
+                {/* {isAddOpen &&
+                    <form onSubmit={() => { return }} className="new-card-form">
+                        <textarea type="text" name="title" value={card.title} onChange={this.handleInput}
+                            className="my-input" placeholder="Enter a card title..." autoFocus required
+                            cols="35" rows="4"></textarea>
+                        <div className="flex">
+                            <button type="submit" onMouseDown={this.onSaveCard} className="submit-card-btn">Add card</button>
+                            <button onClick={this.closeInput} className="close-input-btn" >
+                                <img src="https://res.cloudinary.com/basimgs/image/upload/v1610646476/close_voj9v3.png" alt="" />
+                            </button>
+                        </div>
+                    </form>
+                } */}
+                {!isAddOpen && <button className="add-card-btn" onClick={this.openInput}>
+                    <img src="https://res.cloudinary.com/basimgs/image/upload/v1610625350/plus_ljzrkm.png" alt="" />
+                    <span>Add another card</span></button>}
+            </section>
+        )
     }
 }
---
+++ Frontend/src/cmps/Card/CardAddForm.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { eventBusService } from '../../services/eventBusService'
+import { utilService } from '../../services/utilService'
+import { toggleAddCard } from '../../store/actions/cardAction.js'
+
+export class CardAddForm extends Component {
+  state = {
+    card: {
+      title: ''
+    },
+    isAddOpen: false,
+    groupId: null
+  }
+  componentDidMount() {
+    this.eventBusTerminate = eventBusService.on('open-card-add', this.openCardAdd)
+  }
+
+  componentWillUnmount() {
+    this.eventBusTerminate()
+  }
+
+  openCardAdd = ({ isAddOpen, groupId }) => {
+    if (this.props.groupId === groupId)
+      this.setState({ isAddOpen: isAddOpen, groupId })
+  }
+
+  handleInput = ({ target }) => {
+    const { name } = target
+    const { value } = target
+    this.setState(prevState => {
+      return {
+        card: {
+          ...prevState.card,
+          [name]: value
+        }
+      }
+    })
+  }
+
+  closeInput = () => {
+    this.setState({ isAddOpen: false }, () => {
+      eventBusService.emit('close-card-add', false, this.state.groupId)
+      setTimeout(() => {
+        this.props.onScroll(null, 1000)
+      }, 150)
+    })
+  }
+
+  onSaveCard = (ev) => {
+    ev.preventDefault()
+    if (!this.state.card.title) return
+    const card = { title: this.state.card.title, id: utilService.makeId(), members: [], labels: [] }
+    this.props.onAddCard(card, this.props.groupId)
+    this.setState(
+      {
+        card: { title: '' },
+      }, () => {
+        this.closeInput()
+
+      })
+  }
+
+  render() {
+    const { isAddOpen, card } = this.state
+    return (
+      <div className="add-card-container">
+        { isAddOpen &&
+          <form onSubmit={(ev) => { ev.preventDefault(); return }} className="new-card-form" >
+            <textarea type="text" name="title" value={card.title} onChange={this.handleInput} onBlur={this.closeInput}
+              className="my-input" placeholder="Enter a card title..." autoFocus required
+              cols="35" rows="4"></textarea>
+            <div className="flex" style={{ marginTop: 5 }}>
+              <button type="submit" onMouseDown={this.onSaveCard} className="submit-card-btn">Add card</button>
+              <button onMouseDown={this.closeInput} className="close-input-btn" >
+                <img src="https://res.cloudinary.com/basimgs/image/upload/v1610646476/close_voj9v3.png" alt="" />
+              </button>
+            </div>
+          </form>
+        }
+      </div >
+    )
+  }
+}
+
+
